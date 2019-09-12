90cfeb58754d1d8b0419d219a324baf038bba513
karl-denby <karl.denby@gmail.com>
2019-09-12 09:02:37 +0100

    Example settings and .gitignore create files

--- DiceRoller/app/src/main/java/com.karldenby/diceroller/Constants-example.kt
+++ DiceRoller/app/src/main/java/com.karldenby/diceroller/Constants-example.kt
@@ -4,6 +4,6 @@ package com.karldenby.diceroller
 
 internal object `Constants-example` {
     private const val INSTANCE_ADDRESS = "example-instance-name.region.cloud.realm.io"
-    private const val AUTH_URL = "https://$INSTANCE_ADDRESS/auth"
-    private const val REALM_URL = "realms://$INSTANCE_ADDRESS"
+    const val AUTH_URL = "https://$INSTANCE_ADDRESS/auth"
+    const val REALM_URL = "realms://$INSTANCE_ADDRESS"
 }
