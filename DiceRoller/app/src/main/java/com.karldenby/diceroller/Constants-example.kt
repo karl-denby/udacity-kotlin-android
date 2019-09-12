d219a66492bed854e05ca31c48ee18f3c677bdca
karl-denby <karl.denby@gmail.com>
2019-09-12 08:55:02 +0100

    Example settings

---
+++ DiceRoller/app/src/main/java/com.karldenby/diceroller/Constants-example.kt
@@ -0,0 +1,9 @@
+package com.karldenby.diceroller
+
+// Copy this file, update strings and rename it Constants.kt
+
+internal object `Constants-example` {
+    private const val INSTANCE_ADDRESS = "example-instance-name.region.cloud.realm.io"
+    private const val AUTH_URL = "https://$INSTANCE_ADDRESS/auth"
+    private const val REALM_URL = "realms://$INSTANCE_ADDRESS"
+}
