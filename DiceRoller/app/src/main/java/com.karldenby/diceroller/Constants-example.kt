581df3ff664fcf56aa1bd6ef6125f4d70e51d3ba
karl-denby <karl.denby@gmail.com>
2019-09-14 23:42:47 +0100

    Working example

--- DiceRoller/app/src/main/java/com.karldenby/diceroller/Constants-example.kt
+++ DiceRoller/app/src/main/java/com.karldenby/diceroller/Constants-example.kt
@@ -6,4 +6,6 @@ internal object `Constants-example` {
     private const val INSTANCE_ADDRESS = "example-instance-name.region.cloud.realm.io"
     const val AUTH_URL = "https://$INSTANCE_ADDRESS/auth"
     const val REALM_URL = "realms://$INSTANCE_ADDRESS"
+    const val REALM_USER = "test-user"
+    const val REALM_PASSWORD = "test-password"
 }
--- DiceRoller/app/src/main/java/com.karldenby/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com.karldenby/diceroller/MainActivity.kt
@@ -1,32 +1,30 @@
 package com.karldenby.diceroller
 
+import com.karldenby.diceroller.model.Dice
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
-import kotlin.random.Random
-import io.realm.SyncUser.logInAsync
-import io.realm.SyncUser.Callback
-import java.nio.file.Files.setOwner
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import com.karldenby.diceroller.model.Dice
 import io.realm.*
-import io.realm.com_karldenby_diceroller_model_DiceRealmProxy.insert
-import io.realm.kotlin.where
-import java.sql.Date
 
+import kotlin.random.Random
+
+import io.realm.SyncUser.logInAsync
+import io.realm.log.LogLevel
+import io.realm.log.RealmLog
 
 class MainActivity : AppCompatActivity() {
 
     // assign after inflation
     private lateinit var diceImage: ImageView
     private lateinit var rollButton: Button
-    private lateinit var realm: Realm
+
+    // realm
     private lateinit var realmConfiguration: RealmConfiguration
-    private lateinit var credentials: SyncCredentials
+    private lateinit var realm: Realm
+    private lateinit var realmListener: RealmChangeListener<Realm>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,34 +32,32 @@ class MainActivity : AppCompatActivity() {
 
         // Setup Realm
         Realm.init(this)
-        realm = Realm.getDefaultInstance()
-        credentials = SyncCredentials.usernamePassword("test-user", "test-password")
-        logInAsync(credentials, Constants.AUTH_URL, object : Callback<SyncUser> {
+        RealmLog.setLevel(LogLevel.DEBUG)
+
+        val credentials = SyncCredentials.usernamePassword(Constants.REALM_USER, Constants.REALM_PASSWORD)
+        logInAsync(credentials, Constants.AUTH_URL, object : SyncUser.Callback<SyncUser> {
                 override fun onError(error: ObjectServerError) {
                     Log.e("REALM", "User error $error")
-
                 }
                 override fun onSuccess(result: SyncUser) {
                     Log.i("REALM","We have a user")
                 }
             }
         )
-        val url = "${Constants.REALM_URL}/~/dice-roller"
+
+        val url = Constants.REALM_URL + "/~/dice-roller"
         val data = Dice()
         data.number = 6
+
         realmConfiguration = SyncUser.current().createConfiguration(url)
             .fullSynchronization()
             .schemaVersion(2)
             .initialData { realm ->
-                realm.beginTransaction()
                 realm.copyToRealmOrUpdate(data)
-                realm.commitTransaction()
                 Log.i("REALM", "Inserted default data on $url")
             }
             .build()
-        // Upload will continue in the background even if we
-        // close the Realm immediately.
-        Realm.getInstance(realmConfiguration).close()
+        realm = Realm.getInstance(realmConfiguration)
 
         // now layout has been inflated
         diceImage = findViewById(R.id.dice_image)
@@ -71,20 +67,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun rollDice() {
+    private fun rollDice(result: Int = Random.nextInt(6) + 1) {
 
-        val result: Int = Random.nextInt(6) + 1
+        val cloud = 1
         val data = Dice()
         data.number = result
 
-        val cloud = 1
-
-        realm = Realm.getInstance(realmConfiguration)
+        realm.removeAllChangeListeners()
+        Log.i("REALM", "Roll Dice")
         realm.beginTransaction()
-        realm.where(Dice :: class.java).equalTo("cloud", cloud)
-        realm.copyToRealmOrUpdate(data)
+            realm.where(Dice :: class.java).equalTo("cloud", cloud)
+            realm.copyToRealmOrUpdate(data)
         realm.commitTransaction()
-
 
         val drawableResource = when (result) {
             1 -> R.drawable.dice_1
@@ -97,5 +91,31 @@ class MainActivity : AppCompatActivity() {
 
         diceImage.setImageResource(drawableResource)
 
+        realmListener = RealmChangeListener<Realm> {
+
+            Log.i("REALM", "Data was updated")
+
+            val query = it.where(Dice :: class.java)
+            query.equalTo("cloud", 1 as Int)
+
+            val result = query.findAll()
+
+            Log.i("REALM", "" + result[0]?.number)
+            rollDice(result[0]?.number!!)
+        }
+        realm.addChangeListener(realmListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        realm.removeAllChangeListeners()
+        realm.close()
+        Log.i("REALM","App Paused")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //realm = Realm.getInstance(realmConfiguration)
+        Log.i("REALM","App Paused")
     }
 }
